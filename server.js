f0d4a3bd42bafcfd110f5a03cd9b3341e2bd2b37
Doaa-Hindawii <doaa.hendawy11@gmail.com>
2021-08-25 17:26:32 +0200

    Fix Server.js Errors

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// projectData.last shows the numer of entries. One entry consists of weather data + mood
-projectData.last = -1;
-
 // Require Express to run server and routes
 const express = require('express');
 
 // Dependencies
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Setup Server
+const port = 8000;
 
 // Start up an instance of app
 const app = express();
@@ -19,47 +20,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
 app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
-
-
-// Setup Server
-const port = 8000;
-
-// Spin up the server
-const server = app.listen(port, listening);
-
-// Callback to debug
-  function listening() {
-    console.log("server running"); 
-    console.log(`running on localhost: ${port}`);
-};
 
 //index page
 app.get("/", (req, res) => {
     res.send("index");
 });
 
-//send data from the stored api data.
-app.get("/weather", (req, res) => {
-    res.send([projectData]);
+//post data from api (current weather data)
+app.post("/", (req, res) => {
+  let data = req.body;
+  projectData.current_weather = data.value;
+  projectData.feelings = data.user_feelings;
 });
 
-// POST Route: save weather data and mood
-// Initialize 'Save' route with a callback function to Save data
-app.post('/Save', Save);
+//post data from api (for the 3 hour forcast)
+app.post("/future_weather", (req, res) => {
+  let data = req.body;
+  projectData.future_weather = data;
+});
 
-function Save(req, res) {
-    Entry = {
-        temperature: req.body.temperature,
-        date: req.body.date,
-        userResponse: req.body.userResponse
-    };
-    projectData.last = projectData.last + 1;
-    projectData[projectData.last] = Entry;
-    res.send(projectData);
-}
-  
+//send data from the stored api data.
+app.get("/weather", (req, res) => {
+  res.send([projectData]);
+});
+
+// Spin up the server
+const server = app.listen(port, listening);
+
+// Callback to debug
+function listening() {
+  console.log("server running"); 
+  console.log(`running on localhost: ${port}`);
+};
