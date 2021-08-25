99a6e82f390d08996a32c6624573097ed1941758
Doaa-Hindawii <doaa.hendawy11@gmail.com>
2021-08-25 14:44:15 +0200

    Completed server.js and app.js applied Get&Post

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
+// projectData.last shows the numer of entries. One entry consists of weather data + mood
+projectData.last = -1;
+
 // Require Express to run server and routes
 const express = require('express');
 
@@ -25,8 +28,34 @@ app.use(express.static('website'));
 
 // Setup Server
 const port = 8000;
+
+// Spin up the server
 const server = app.listen(port, listening);
 
+//index page
+app.get("/", (req, res) => {
+    res.send("index");
+  });
+  
+  //post data from api (current weather data)
+  app.post("/", (req, res) => {
+    let data = req.body;
+    projectData.current_weather = data.value;
+    projectData.feelings = data.user_feelings;
+  });
+  
+  //post data from api (for the 3 hour forcast)
+  app.post("/future_weather", (req, res) => {
+    let data = req.body;
+    projectData.future_weather = data;
+  });
+  
+  //send data from the stored api data.
+  app.get("/weather", (req, res) => {
+    res.send([projectData]);
+  });
+
+  // Callback to debug
 function listening() {
     console.log("server running"); 
     console.log(`running on localhost: ${port}`);
--- website/app.js
+++ website/app.js
@@ -1,5 +1,158 @@
 /* Global Variables */
 
+// Base URL and My API Key from OpenWeatherMap API
+const BaseURL = 'https://api.openweathermap.org/data/2.5/weather?units=metric&zip=';
+const MyApiKey ='&appid=173ff8f535f3ee441c0f90792e8acbdb';
+
+// Event listener callback function
+document.getElementById('generate').addEventListener('click', performAction);
+function performAction(e) {
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+//get the user zipcode input
+const userInfo = () => {
+  const errors = document.querySelectorAll(".error");
+  const zip = parseInt(document.querySelector("#zip").value, 10);
+  if (isNaN(zip)) {
+    error("InCorrect ZipCode!");
+  } else if (zip == undefined) {
+    error("Please, type valid ZipCode!");
+  } else {
+    if (errors.length > 0) {
+      errors.forEach(element => element.parentNode.removeChild(element));
+    }
+    getWeather(zip, document.querySelector("#feelings").value);
+  }
+};
+
+//getWeather -> fetch data from the api and post the data to the server
+const getWeather = async (zip, user_feelings) => {
+  let [current_weather, future_weather] = await Promise.all([
+    fetch(BaseURL + zip + MyApiKey)
+  ]);
+
+  current_weather.json().then(value => {
+    postData("/", { value, user_feelings });
+  });
+  future_weather.json().then(value => {
+    postData("/future_weather", value);
+    //after posting the data, get the data from the server
+    add_server_data();
+  });
+};
+
+//post data in json format in cors mode to the server
+const postData = async (url = "", data) => {
+  const res = await fetch(url, {
+    method: "post",
+    mode: "cors",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+  try {
+    const newData = await res.json();
+    console.log(newData);
+    return newData;
+  } catch (err) {
+    console.log("Error " + err);
+  }
+};
+
+//convert kelvin to fahrenheit
+const kelvin_to_fahrenheit = kelvin_temp =>
+  Math.round(kelvin_temp * 1.8 - 459.67);
+
+//error message to the DOM
+const error = mesg => {
+  const zip = document.querySelector(".zip");
+  const div = element_creator("div", "error");
+  div.textContent = mesg;
+  zip.appendChild(div);
+};
+//creates a given element and a class
+const element_creator = (element, class_name) => {
+  const element_holder = document.createElement(element);
+  element_holder.className = class_name;
+  return element_holder;
+};
+
+//convert unix timestamp to UTC time and return the hour
+const unix_timestamp_converter = unix_timestamp => {
+  const date_info = new Date(unix_timestamp * 1000);
+  return date_info.getHours();
+};
+
+//convert military hour in to stand hours
+const military_time_converter = time_hour => {
+  if (time_hour > 0 && time_hour <= 12) {
+    return time_hour;
+  } else if (time_hour > 12) {
+    return time_hour - 12;
+  } else if (time_hour == 0) {
+    time_hour = 12;
+    return time_hour;
+  }
+};
+
+//dynamically add the future weather data to the UI
+const weather_append = weather_data => {
+  const entry = document.querySelector(".entry");
+  for (weather of weather_data) {
+    let entryHolder = element_creator("div", "entryHolder");
+    let icon = element_creator("div", "icon");
+    let temp = element_creator("div", "temp");
+    let weather_date = element_creator("div", "date");
+    const ampm = unix_timestamp_converter(weather.dt) >= 12 ? "pm" : "am";
+    weather_date.innerHTML = `<p>${military_time_converter(
+      unix_timestamp_converter(weather.dt)
+    ) + ampm}</p>`;
+    icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png" class="current_weather_icon" />`;
+    temp.innerHTML = `<p>${kelvin_to_fahrenheit(weather.main.temp)}&deg</p>`;
+    entryHolder.appendChild(icon);
+    entryHolder.appendChild(temp);
+    entryHolder.appendChild(weather_date);
+    entry.appendChild(entryHolder);
+  }
+};
+
+//gets weather data from the server and dynamically updates the UI
+const add_server_data = async () => {
+  const feel_div_children = document.querySelector(".feel").children;
+  await fetch("/weather").then(server_data => {
+    server_data.json().then(data => {
+      if (data[0].current_weather.cod == "400") {
+        error(data[0].current_weather.message);
+      } else {
+        feel_div_children[5].innerHTML = `<p>Feeling: ${data[0].feelings}</p>`;
+        feel_div_children[0].children[0].textContent =
+          data[0].current_weather.name;
+        feel_div_children[1].innerHTML = `<img src="http://openweathermap.org/img/wn/${data[0].current_weather.weather[0].icon}@2x.png" id="current_weather_icon" />`;
+        feel_div_children[3].innerHTML = `<p>${kelvin_to_fahrenheit(
+          data[0].current_weather.main.temp
+        )}&deg</p>`;
+        feel_div_children[4].innerHTML = `<p>Feels like: ${kelvin_to_fahrenheit(
+          data[0].current_weather.main.feels_like
+        )}&deg</p>`;
+        feel_div_children[2].innerHTML = `<p>${data[0].current_weather.weather[0].main}</p>`;
+        const entryHolder = document.querySelectorAll(".entryHolder");
+        if (entryHolder.length > 0) {
+          entryHolder.forEach(element =>
+            element.parentNode.removeChild(element)
+          );
+        }
+        weather_append(data[0].future_weather.list.splice(0, 5));
+      }
+    });
+  });
+};
+
+//listen for a click on the search button
+const search = document
+  .querySelector("#generate")
+  .addEventListener("click", userInfo);
